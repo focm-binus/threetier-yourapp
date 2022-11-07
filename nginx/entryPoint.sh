95fd6aa34fa8c5df199abda6a39fae5c811d1227
ardimasp <ardimasandipurwita@outloo.com>
2022-11-07 19:11:28 +0700

    fix env var

--- nginx/entryPoint.sh
+++ nginx/entryPoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sed -i "s/SERVER_ADDRESS/$SERVER_ADDRESS/g" /etc/nginx/nginx.conf
+sed -i "s/BACKEND_ADDRESS/$BACKEND_ADDRESS/g" /etc/nginx/nginx.conf
+sed -i "s/FRONTEND_ADDRESS/$FRONTEND_ADDRESS/g" /etc/nginx/nginx.conf
 
 exec "$@"
